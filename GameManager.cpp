599773437d01bc1996dd16ad9f1e47c189911323
WM <graczyk.mateusz@interia.pl>
2019-07-04 14:08:23 +0200

    add: check availability of the field and protection against wrong input

--- GameManager.cpp
+++ GameManager.cpp
@@ -2,54 +2,90 @@
 #include <memory>
 #include <time.h>
 #include <stdlib.h>
+#include <string>
 #include "GameManager.h"
 #include "Map.h"
 
 using std::cout;
 using std::cin;
 
-void GameManager::inputControl() {
-	if(tour == 0) {
+void GameManager::inputControl(std::shared_ptr<Map> map) {
+	int selectedField;
+
+	//Sets whose round it is 
+	if(round == 0) {
 		srand(time(NULL));
-		tour = rand() % 2;
+		round = rand() % 2;
 	} 
-	if(tour % 2 != 0) {
+	if(round % 2 != 0) {
 		cout << "Player X: ";
 	}
 	else {
 		cout << "Player O: ";
 	}
-	cin >> area;
-	tour++;
+	
+	//Protection against wrong input
+	do {
+		cin >> selectedField;
+		if(checkFieldAvailability(selectedField, map)) {
+			field = selectedField;
+		}
+		else {
+			field = -1;
+			cout << "Wrong field, select again: ";
+		}
+	}while((selectedField < 0 && selectedField > 8) || field == -1);
+	
+	round++;
 }
+
 void GameManager::setX(std::shared_ptr<Map> map) {
+	auto* ptrToMap = map->getMap();
 	for(int i = 0; i < 9; i += 2) {
-		
+		(*ptrToMap)[field][i] = "#";
 	}		
 }
+
 void GameManager::setO(std::shared_ptr<Map> map) {
+	auto* ptrToMap = map->getMap();
 	for(int i = 0; i < 9; i++) {
 		if(i != 4) {
-			
+			(*ptrToMap)[field][i] = "#";
+		}
+		else {
+			(*ptrToMap)[field][i] = " ";
 		}	
 	}
 }
-std::string GameManager::checkTour() {
-	if(tour % 2 != 0) {
+
+std::string GameManager::checkRound() {
+	if(round % 2 != 0) {
 		return "X";
 	}
 	else {
 		return "O";	
 	}
 }
+
+bool GameManager::checkFieldAvailability(int selectedField, std::shared_ptr<Map> map) {	
+	auto* ptrToMap = map->getMap();
+	if((*ptrToMap)[selectedField][0] == "#") { //Position '0' is common for X and O
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
 void GameManager::setIntoMap(std::shared_ptr<Map> map) {
-	if(checkTour() == "X") {
+	if(checkRound() == "X") {
 		setX(map);
 	}
 	else {
 		setO(map);
 	}
 }
+
 bool GameManager::checkWin() {
 	return true;
 }
--- GameManager.h
+++ GameManager.h
@@ -6,13 +6,18 @@ class GameManager {
 	public:
 	GameManager() = default;
 	~GameManager() = default;
-	void inputControl();
+	void inputControl(std::shared_ptr<Map> map);
 	void setIntoMap(std::shared_ptr<Map> map);	
     	bool checkWin();
+
 	private:
+	/*Methods*/
 	void setX(std::shared_ptr<Map> map);
 	void setO(std::shared_ptr<Map> map);
-	std::string checkTour();
-	short area;
-	unsigned short tour = 0;
+	std::string checkRound();
+	bool checkFieldAvailability(int selectedField, std::shared_ptr<Map> map);
+
+	/*Variables*/
+	short field;
+	unsigned short round = 0;
 };
